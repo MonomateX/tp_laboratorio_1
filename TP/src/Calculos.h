cbf424ae1379d0aa6152761d1dbbb1f6b2c38fab
unknown <MonomateXII@gmail.com>
2021-04-17 18:51:48 -0300

    Subiendo el TP n°1

---
+++ TP/src/Calculos.h
@@ -0,0 +1,52 @@
+/*
+ * Calculos.h
+ *
+ *  Created on: 17 abr. 2021
+ *      Author: monom
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+
+#include <stdio.h>
+
+/** \brief Realiza una suma entre los numeros ingresados
+ *
+ *  \param float Primer numero ingresado
+ *  \param float Segundo numero ingresado
+ *  \return float Devuelve el resultado de la suma de ambos numeros
+ **/
+float SumarNumeros(float,float);
+
+/** \brief Realiza una resta entre los numeros ingresados
+ *
+ *  \param float Primer numero ingresado
+ *  \param float Segundo numero ingresado
+ *  \return float Devuelve el resultado de la resta de ambos numeros
+ **/
+float RestarNumeros(float,float);
+
+/** \brief Realiza una division entre los numeros ingresados
+ *
+ *  \param float Primer numero ingresado
+ *  \param float Segundo numero ingresado
+ *  \return float Devuelve el resultado de la division o 0 en caso de no poder realizar la operacion
+ **/
+float DividirNumeros(float,float);
+
+/** \brief Realiza una multiplicacion entre los numeros ingresados
+ *
+ *  \param float Primer numero ingresado
+ *  \param float Segundo numero ingresado
+ *  \return float Devuelve el resultado de la multiplicacion de ambos numeros
+ **/
+float MultiplicarNumeros(float,float);
+
+/** \brief Calcula el factorial del numero ingresado
+ *
+ *  \param float Numero a sacar el factorial
+ *  \return int Devuelve el factorial del numero ingresado
+ **/
+int FactorialNumero(float);
+
+#endif /* CALCULOS_H_ */
---
+++ TP/src/TP.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : TP.c
+ Author      : Piuca Ricardo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Menu.h"
+#include "Calculos.h"
+#define T 0
+
+int main(void) {
+	int opcion;
+	eNumeros operacion = {T,T,T,T,T,T,T,T,T};
+
+	setbuf(stdout, NULL);
+
+	do
+	{
+		opcion = MostrarMenu(operacion);
+	    operacion = EjecutarOpcion(operacion, opcion);
+	}while(opcion != 5);
+
+	return EXIT_SUCCESS;
+}
